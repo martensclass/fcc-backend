1293e3ae0b9b3b5b3a8c52b7b2ebac64615642f7
John Martens <jmartens@scdsb.on.ca>
2016-06-14 18:30:51 +0000

    Completed Timestamp Micro-Serive project

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 var express = require('express');
 
 var app = express();
+var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/index.html');
 });
 
+app.get('/:id', function (req, res) {
+  var id = req.params.id;
+  var d,u, n;
+  //what if id is a number?  Then UNIX time it is
+ if(isNaN(Number(id))==false){
+   d = new Date(Number(id)*1000);
+   u = d.getTime()/1000;
+   n = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+ }
+ else{ //try to make a date out of it
+    d = new Date(id);
+     u = d.getTime()/1000;
+     if(months[d.getMonth()]===undefined)
+      n=null;
+    else{ //make the unix and natural dates
+        n = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+    }
+ }
+ res.json({unix: u, natural: n});
+ 
+});
+
 app.listen(8080, function () {
     console.log('Listening on port 8080...');
 });
+
